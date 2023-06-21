5e2d8cd28a1f844e08b306d42e891c6f4f937bc1
SamBryce-Smith <samuelbrycesmith@gmail.com>
2023-06-21 10:43:38 +0100

    add scripts, conda env to quantify last exons

---
+++ postmortem/summarise_quant.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# Need this conda env active - envs/papa_r.yaml
+
+script="scripts/tx_to_polya_quant.R"
+sample_tbl="data/liu_facs_papa_sample_sheet.csv"
+salmon_dir="data/placeholder"
+tx2le="processed/2023-06-21_cryptics_plus_decoys.decoys_full.tx2le.tsv"
+le2gene="processed/2023-06-21_cryptics_plus_decoys.decoys_full.tx2gene.tsv"
+out_prefix="processed/2023-06-21_summarised_pas_quantification_cryptics"
+
+Rscript $script \
+-s $sample_tbl \
+-d $salmon_dir \
+-t $tx2le \
+-g $le2gene \
+-o $out_prefix &> ${out_prefix}.tx_to_polya_quant.log
