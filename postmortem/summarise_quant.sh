35398f209919a27840b8b846bd885569d01eab96
SamBryce-Smith <samuelbrycesmith@gmail.com>
2023-06-22 14:33:39 +0100

    proper script to calculate ppau for decoy quantified transcripts

--- postmortem/summarise_quant.sh
+++ postmortem/summarise_quant.sh
@@ -4,10 +4,10 @@
 
 script="scripts/tx_to_polya_quant.R"
 sample_tbl="data/liu_facs_papa_sample_sheet.csv"
-salmon_dir="data/placeholder"
-tx2le="processed/2023-06-21_cryptics_plus_decoys.decoys_full.tx2le.tsv"
-le2gene="processed/2023-06-21_cryptics_plus_decoys.decoys_full.tx2gene.tsv"
-out_prefix="processed/2023-06-21_summarised_pas_quantification_cryptics"
+salmon_dir="data/liu_facs/2023-06-21_gencode_v40_cryptics_decoys_full/salmon_quant"
+tx2le="processed/2023-06-22_cryptics_plus_decoys.decoys_full_fix_tx2le.tx2le.tsv"
+le2gene="processed/2023-06-22_cryptics_plus_decoys.decoys_full_fix_tx2le.tx2gene.tsv"
+out_prefix="processed/2023-06-21_summarised_pas_quantification_cryptics_fix_tx2le"
 
 Rscript $script \
 -s $sample_tbl \
