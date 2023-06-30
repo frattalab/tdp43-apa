3b9f2eb276ad48d3395f477d9c521fc279cef648
SamBryce-Smith <samuelbrycesmith@gmail.com>
2023-06-30 17:03:44 +0100

    updated plot split by event type

--- postmortem/scripts/plot_liu_facs.R
+++ postmortem/scripts/plot_liu_facs.R
@@ -31,12 +31,22 @@ p_le2name <- "processed/2023-06-22_cryptics_plus_decoys.decoys_full_fix_tx2le.le
 
 p_sample_tbl <- "data/liu_facs_papa_sample_sheet.csv"
 
+# due to some ID mapping errors and overlapping event types, performed manual curation of event types 
+# (some due to e.g. overlapping isoforms being grouped together - focused on event type of cryptic by expression region)
+p_event_type_na_mc <- "data/liu_facs_event_type_manual_curation.tsv"
+p_event_type_complex_mc <- "data/cryptics_summary_complex_manual_curation.tsv"
+
+
 ppau <- read_tsv(p_ppau)
 tx2le <- read_tsv(p_tx2le)
 le2name <- read_tsv(p_le2name) %>%
   # wasn't smart with gene_name collapsing, often get duplicates...
   mutate(gene_name = collapse_names(gene_name))
 sample_tbl <- read_csv(p_sample_tbl)
+
+event_type_na_mc <- read_tsv(p_event_type_na_mc)
+event_type_complex_mc <- read_tsv(p_event_type_complex_mc)
+
 
 ## 
 dexseq <- read_tsv(p_dexseq)
@@ -56,10 +66,48 @@ cryp_le_ids <- tx2le %>%
 
 # extract event type annotation
 le2event <- dexseq %>%
-  distinct(le_id, simple_event_type) %>%
-  group_by(le_id) %>%
+  distinct(le_id, simple_event_type, gene_name) %>%
+  group_by(gene_name, le_id) %>%
   summarise(simple_event_type = paste(unique(sort(simple_event_type)), collapse = ",")) %>%
   ungroup()
+
+# add plot_le_id
+le2event <- mutate(le2event, plot_le_id = paste(gene_name, str_split_i(le_id, "_", 2), sep = "_"))
+  
+
+# have manual curation of event types
+# combine the two tables, filtering for passing events
+event_type_complex_mc <- event_type_complex_mc %>%
+  mutate(plot_le_id = paste(gene_name, str_split_i(le_id, "_", 2), sep = "_"))
+
+event_type_complex_mc_p <- event_type_complex_mc %>%
+  select(le_id, plot_le_id, event_manual_validation, simple_event_type = manual_simple_event_type) %>%
+  # some events did not pass manual curation - filter out
+  filter(event_manual_validation == "yes")
+
+event_type_na_mc_p <- event_type_na_mc %>%
+  filter(event_manual_validation == "yes") %>%
+  select(plot_le_id, simple_event_type = manual_simple_event_type, event_manual_validation)
+
+event_type_mc <- bind_rows(event_type_na_mc_p, event_type_complex_mc_p)
+
+le2event <- bind_rows("original" = le2event, "manual" = event_type_mc, .id = "source") %>%
+  group_by(plot_le_id) %>%
+  # filter(any(source == "manual")) %>%
+  # if le_id contains two annotations, manual will come first (i.e. manual curation), so prefer over complex annotation
+  # if only in original, will keep whatever annotation provided there
+  arrange(source, .by_group = T) %>%
+  slice_head(n = 1) %>% 
+  ungroup()
+
+# above aslo has purpose of adding missign le_ids to tabel (due to mismapping)
+# need to double check that manually curated events are remvoed from le2event
+
+event_type_mc_f_ids <- c(event_type_complex_mc %>% filter(event_manual_validation != "yes") %>% pull(plot_le_id),
+                         event_type_na_mc %>% filter(event_manual_validation != "yes") %>% pull(plot_le_id)
+  )
+
+le2event <- filter(le2event, !plot_le_id %in% event_type_mc_f_ids)
 
 count(le2event, simple_event_type)
 # A tibble: 5 × 2
@@ -73,27 +121,26 @@ count(le2event, simple_event_type)
 
 
 # join in gene name for each le_id
+# dfine a more readable isoform id - gene_name suffixed with isoform number
 # annotate whether cryptic/nto cryptic
 ppau <- left_join(ppau, distinct(le2name, .keep_all = T), by = "le_id") %>% 
-  relocate(gene_name, .after = gene_id) %>%
+  mutate(plot_le_id = paste(gene_name, str_split_i(le_id, "_", 2), sep = "_")) %>%
+  relocate(gene_name, plot_le_id, .after = gene_id) %>%
   # don't need these columns
   select(-starts_with("mean"), -starts_with("delta")) %>%
   mutate(cryptic_status = if_else(le_id %in% cryp_le_ids, T, F)) %>%
-  left_join(le2event, by = "le_id")
+  left_join(select(le2event, plot_le_id, ev_le_id = le_id, simple_event_type, source),
+            by = "plot_le_id") %>%
+    relocate(simple_event_type, .after = plot_le_id) %>%
+  # make sure remove le_ids if manually curated and present in ppau df
+  filter(!(plot_le_id %in% event_type_mc_f_ids))
 
-# dfine a more readable isoform id - gene_name suffixed with isoform number
-ppau <- ppau %>%
-  group_by(gene_name) %>%
-  arrange(le_id, .by_group = T) %>%
-  mutate(le_n = row_number(),
-         plot_le_id = paste(gene_name, le_n, sep = "_")) %>%
-  ungroup() %>%
-  select(-le_n)
+
   
 
 # join in metadata from sample table (converting to long format with row per sample and isoform)
 ppau <- ppau %>%
-  pivot_longer(cols = -all_of(c("le_id", "gene_id", "gene_name", "cryptic_status", "plot_le_id", "simple_event_type")),
+  pivot_longer(cols = -all_of(c("le_id", "gene_id", "gene_name", "cryptic_status", "plot_le_id", "simple_event_type", "ev_le_id", "source")),
                names_to = "sample_name",
                values_to = "ppau") %>%
   left_join(select(sample_tbl, -path, -fastq1, -fastq2),
@@ -115,12 +162,12 @@ ppau_delta_paired <- ppau %>%
 
 # calculate mean & median delta PAUs across samples for each isoform
 ppau_delta_paired_median_all <- ppau_delta_paired %>%
-  group_by(le_id) %>%
+  group_by(plot_le_id) %>%
   summarise(mean_paired_delta_ppau = mean(paired_delta_ppau_neg_pos),
             median_paired_delta_ppau = median(paired_delta_ppau_neg_pos),
             ) %>%
   ungroup() %>%
-  left_join(distinct(ppau_delta_paired, le_id, gene_id, gene_name, cryptic_status, plot_le_id), by = "le_id") %>%
+  left_join(distinct(ppau_delta_paired, le_id, gene_id, gene_name, cryptic_status, plot_le_id), by = "plot_le_id") %>%
   relocate(all_of(c("mean_paired_delta_ppau", "median_paired_delta_ppau"
                     )),
            .after = last_col()
@@ -129,7 +176,7 @@ ppau_delta_paired_median_all <- ppau_delta_paired %>%
 
 # calculate mean & median delta PAUs across samples for each isoform
 ppau_delta_paired_median_disease <- ppau_delta_paired %>%
-  group_by(le_id, disease) %>%
+  group_by(plot_le_id, disease) %>%
   summarise(mean_paired_delta_ppau = mean(paired_delta_ppau_neg_pos),
             median_paired_delta_ppau = median(paired_delta_ppau_neg_pos),
             rank_mean_up = min_rank(desc(mean_paired_delta_ppau)),
@@ -148,9 +195,9 @@ mutate(rank_mean_up = min_rank(desc(mean_paired_delta_ppau)),
        rank_median_up = min_rank(desc(median_paired_delta_ppau))) %>%
   arrange(rank_median_up, rank_mean_up)
 
-ppau_order_cryp_median <- ppau_order_cryp %>%
-  arrange(rank_median_up) %>%
-  pull(le_id)
+# ppau_order_cryp_median <- ppau_order_cryp %>%
+#   arrange(rank_median_up) %>%
+#   pull(le_id)
 
 ppau_order_cryp_median_gn <- ppau_order_cryp %>%
   arrange(rank_median_up) %>%
@@ -172,44 +219,7 @@ ppau_delta_paired_median_all %>%
 ppau_delta_paired_cryp <- ppau_delta_paired %>%
   filter(cryptic_status)
 
-ppau_delta_paired_cryp %>%
-  mutate(le_id = factor(le_id, levels = rev(ppau_order_cryp_median)),
-         plot_le_id = factor(plot_le_id, levels = rev(ppau_order_cryp_median_gn))
-         ) %>%
-  ggplot(aes(x = patient_id,
-             y = le_id,
-             fill = paired_delta_ppau_neg_pos)) +
-  geom_tile() +
-  scale_fill_gradientn(name = "Sample-wise dPPAU (TDPneg - TDPpos)",
-                       colours = c("#998ec3", "#f7f7f7", "#f1a340"),
-                       limits = c(-1, 1)) +
-  theme_bw() +
-  labs(title = "Cell line cryptic last exons - relative usage change in FACS nuclei",
-       x = "Sample ID",
-       y = "Gene name") +
-  theme(axis.text.x = element_text(angle = 45,hjust = 1),
-        axis.text.y = element_text(size=rel(0.82)))
-
-ppau_delta_paired_cryp %>%
-  mutate(le_id = factor(le_id, levels = rev(ppau_order_cryp_median)),
-         plot_le_id = factor(plot_le_id, levels = rev(ppau_order_cryp_median_gn))
-  ) %>%
-  ggplot(aes(x = patient_id,
-             y = plot_le_id,
-             fill = paired_delta_ppau_neg_pos)) +
-  geom_tile() +
-  scale_fill_gradientn(name = "Sample-wise dPPAU (TDPneg - TDPpos)",
-                       colours = c("#998ec3", "#f7f7f7", "#f1a340"),
-                       limits = c(-1, 1)) +
-  theme_bw() +
-  labs(title = "Cell line cryptic last exons - relative usage change in FACS nuclei",
-       x = "Sample ID",
-       y = "Gene name") +
-  theme(axis.text.x = element_text(angle = 45,hjust = 1),
-        axis.text.y = element_text(size=rel(0.82)))
-
-
-
+# add event
 # at least 5 % median delta
 
 ppau_order_cryp_median_gn_5 <- ppau_order_cryp %>%
@@ -217,58 +227,111 @@ ppau_order_cryp_median_gn_5 <- ppau_order_cryp %>%
   pull(plot_le_id)
 
 
-ppau_delta_paired_cryp %>%
-  filter(plot_le_id %in% ppau_order_cryp_median_gn_5) %>%
-  mutate(
-         plot_le_id = factor(plot_le_id, levels = rev(ppau_order_cryp_median_gn_5))
-  ) %>%
-  ggplot(aes(x = patient_id,
-             y = plot_le_id,
-             fill = paired_delta_ppau_neg_pos)) +
-  geom_tile() +
-  scale_fill_gradientn(name = "Sample-wise dPPAU (TDPneg - TDPpos)",
-                       colours = c("#998ec3", "#f7f7f7", "#f1a340"),
-                       limits = c(-1, 1)) +
-  theme_bw() +
-  labs(title = "Cell line cryptic last exons - relative usage change in FACS nuclei",
-       x = "Sample ID",
-       y = "Gene name") +
-  theme(axis.text.x = element_text(angle = 45,hjust = 1),
-        axis.text.y = element_text(size=rel(0.82)))
-
-
-ppau_delta_paired_cryp %>%
+plot_df_median_5_delta <- ppau_delta_paired_cryp %>%
   filter(plot_le_id %in% ppau_order_cryp_median_gn_5) %>%
   mutate(
     plot_le_id = factor(plot_le_id, levels = rev(ppau_order_cryp_median_gn_5)),
-    simple_event_type = factor(simple_event_type, levels = c("spliced", "bleedthrough", "distal_3utr_extension",
-                                                             "bleedthrough,spliced",
-                                                             "bleedthrough,distal_3utr_extension",
-                                                             NA
-                                                             )
-                               )
-  ) %>%
+    # simple_event_type = factor(simple_event_type, levels = c("spliced", "bleedthrough", "distal_3utr_extension",
+    #                                                          "bleedthrough,spliced",
+    #                                                          "bleedthrough,distal_3utr_extension",
+    #                                                          NA
+    #                                                          )
+    #                            ),
+    plot_event_type = case_when(simple_event_type == "spliced" ~ "AS-ALE",
+                                simple_event_type == "bleedthrough" ~ "Bleedthrough-ALE",
+                                simple_event_type == "distal_3utr_extension" ~ "3'UTR-ALE",
+                                str_detect(simple_event_type, ",") ~ "Complex"),
+    plot_event_type = factor(plot_event_type, levels = c("AS-ALE", "Bleedthrough-ALE", "3'UTR-ALE", "Complex", NA))
+  )
+
+plot_df_median_5_delta %>%
   ggplot(aes(x = patient_id,
              y = plot_le_id,
              fill = paired_delta_ppau_neg_pos)) +
-  facet_wrap("~ simple_event_type", scales = "free_y") +
+  facet_wrap("~ plot_event_type", scales = "free_y") +
   geom_tile() +
   scale_fill_gradientn(name = "Sample-wise dPPAU (TDPneg - TDPpos)",
                        colours = c("#998ec3", "#f7f7f7", "#f1a340"),
                        limits = c(-1, 1)) +
-  theme_bw() +
-  labs(title = "Cell line cryptic last exons - relative usage change in FACS nuclei",
+  theme_bw(base_size = 16) +
+  labs(subtitle = "Cryptic last exons with median dPPAU > 0.05",
        x = "Sample ID",
        y = "Gene name") +
-  theme(axis.text.x = element_text(angle = 45,hjust = 1),
-        axis.text.y = element_text(size=rel(0.82)))
+  theme(axis.text.x = element_text(angle = 90),
+        legend.position = "bottom",
+        legend.key.size = unit(8, "mm")
+        )
 
 
-ggsave("example_liu_facs_split_by_event_type.png",
+ggsave("2023-06-30_liu_facs_cryptic_median_delta_05_event_type_facet.png",
        height = 8,
        width = 12,
        units = "in",
        dpi = "retina")
+
+
+
+# ppau_delta_paired_cryp %>%
+#   filter(plot_le_id %in% ppau_order_cryp_median_gn_5) %>%
+#   mutate(
+#     plot_le_id = factor(plot_le_id, levels = rev(ppau_order_cryp_median_gn_5))
+#   ) %>%
+#   ggplot(aes(x = patient_id,
+#              y = plot_le_id,
+#              fill = paired_delta_ppau_neg_pos)) +
+#   geom_tile() +
+#   scale_fill_gradientn(name = "Sample-wise dPPAU (TDPneg - TDPpos)",
+#                        colours = c("#998ec3", "#f7f7f7", "#f1a340"),
+#                        limits = c(-1, 1)) +
+#   theme_bw() +
+#   labs(title = "Cell line cryptic last exons - relative usage change in FACS nuclei",
+#        x = "Sample ID",
+#        y = "Gene name") +
+#   theme(axis.text.x = element_text(angle = 45,hjust = 1),
+#         axis.text.y = element_text(size=rel(0.82)))
+
+
+# ppau_delta_paired_cryp %>%
+#   mutate(le_id = factor(le_id, levels = rev(ppau_order_cryp_median)),
+#          plot_le_id = factor(plot_le_id, levels = rev(ppau_order_cryp_median_gn))
+#          ) %>%
+#   ggplot(aes(x = patient_id,
+#              y = le_id,
+#              fill = paired_delta_ppau_neg_pos)) +
+#   geom_tile() +
+#   scale_fill_gradientn(name = "Sample-wise dPPAU (TDPneg - TDPpos)",
+#                        colours = c("#998ec3", "#f7f7f7", "#f1a340"),
+#                        limits = c(-1, 1)) +
+#   theme_bw() +
+#   labs(title = "Cell line cryptic last exons - relative usage change in FACS nuclei",
+#        x = "Sample ID",
+#        y = "Gene name") +
+#   theme(axis.text.x = element_text(angle = 45,hjust = 1),
+#         axis.text.y = element_text(size=rel(0.82)))
+# 
+# ppau_delta_paired_cryp %>%
+#   mutate(le_id = factor(le_id, levels = rev(ppau_order_cryp_median)),
+#          plot_le_id = factor(plot_le_id, levels = rev(ppau_order_cryp_median_gn))
+#   ) %>%
+#   ggplot(aes(x = patient_id,
+#              y = plot_le_id,
+#              fill = paired_delta_ppau_neg_pos)) +
+#   geom_tile() +
+#   scale_fill_gradientn(name = "Sample-wise dPPAU (TDPneg - TDPpos)",
+#                        colours = c("#998ec3", "#f7f7f7", "#f1a340"),
+#                        limits = c(-1, 1)) +
+#   theme_bw() +
+#   labs(title = "Cell line cryptic last exons - relative usage change in FACS nuclei",
+#        x = "Sample ID",
+#        y = "Gene name") +
+#   theme(axis.text.x = element_text(angle = 45,hjust = 1),
+#         axis.text.y = element_text(size=rel(0.82)))
+# 
+# 
+
+
+
+
 
 
 # use ggside to add gender (& possibly gene expression) as side tiles to heatmap, event type
