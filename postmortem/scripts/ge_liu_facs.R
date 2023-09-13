9f1cbb9f5c25c63dcf7fe916a116228e750ef872
SamBryce-Smith <samuelbrycesmith@gmail.com>
2023-09-13 13:15:14 +0100

    output plots for all 3 types of mean calc

--- postmortem/scripts/ge_liu_facs.R
+++ postmortem/scripts/ge_liu_facs.R
@@ -1,13 +1,86 @@
 library(tidyverse)
 library(ggrepel)
 library(ggpubr)
-
+library(glue)
 
 calc_means <- function(df, sample_cols, out_col) {
   
   df[, out_col] <- rowMeans(df[, sample_cols])
   df
 
+}
+
+prep_means_df_plot <- function(df, mean_col) {
+  
+  df %>%
+    mutate(gene_name = fct_reorder(gene_name, !!sym(mean_col)),
+           plot_gn = if_else(enr_cryp, gene_name, ""),
+           plot_x = log2(!!sym(mean_col) + 1)) 
+}
+
+#' Generate a ggplot of log2 transformed counts for each gene in ascending order
+#' must contain gene_name, enr_cryp column (T/F - whether to highlight) & a column of your choice with mean value
+base_dotplot <- function(df, mean_col, plot_title = "Cryptic-containing gene expression") {
+  
+  df %>%
+    prep_means_df_plot(., mean_col) %>%
+    ggplot(aes(x = plot_x, y = gene_name, colour = enr_cryp, label = plot_gn)) +
+    geom_point(size = rel(0.3)) +
+    geom_vline(xintercept = log2(11), linetype = "dashed") +
+    geom_vline(xintercept = log2(26), linetype = "dashed") +
+    geom_vline(xintercept = log2(51), linetype = "dashed") +
+    scale_colour_manual(values = c("#bdbdbd", "#d95f02")) +
+    theme_classic(base_size = 14) +
+    labs(title = plot_title,
+         subtitle = "Vertical lines correpond to a mean of 10, 25 & 50 reads respectively",
+         x = "log2(mean count + 1)",
+         y = "Gene",
+         colour = "Passes enrichment criteria") +
+    theme(axis.text.y = element_blank(),
+          axis.ticks.y = element_blank(),
+          legend.position = "top")
+  
+}
+
+#'
+#' must contain gene_name, enr_cryp column (T/F - whether to highlight) & a column of your choice with mean value
+ge_density_plot <- function(df, mean_col, plot_title = "Cryptic-containing gene expression") {
+  
+  df %>%
+    prep_means_df_plot(., mean_col) %>%
+    ggplot(aes(x = plot_x, fill = enr_cryp, colour = enr_cryp)) +
+    geom_density(alpha = 0.5) +
+    scale_colour_manual(values = c("#bdbdbd", "#d95f02")) +
+    scale_fill_manual(values = c("#bdbdbd", "#d95f02")) +
+    theme_classic(base_size = 14) +
+    labs(title = plot_title,
+         x = "log2(mean count + 1)",
+         fill = "Passes enrichment criteria",
+         colour = "Passes enrichment criteria"
+    ) +
+    theme(legend.position = "top")
+  
+}
+
+#' must contain gene_name, enr_cryp column (T/F - whether to highlight) & a column of your choice with mean value
+ge_boxplot <- function(df, mean_col, plot_title = "Cryptic-containing gene expression") {
+
+  df %>%
+    prep_means_df_plot(., mean_col) %>%
+    ggplot(aes(y = plot_x, x = enr_cryp, colour = enr_cryp)) +
+    geom_boxplot(outlier.shape = NA) + 
+    geom_jitter() +
+    ggpubr::stat_compare_means() +
+    scale_colour_manual(values = c("#bdbdbd", "#d95f02")) +
+    scale_fill_manual(values = c("#bdbdbd", "#d95f02")) +
+    theme_classic(base_size = 14) +
+    labs(title = plot_title,
+         x = "Passes enrichment criteria",
+         y = "log2(mean count + 1)"
+    )  +
+    guides(fill = "none",
+           colour = "none")
+  
 }
 
 # decoy tx quantification
@@ -116,133 +189,84 @@ cryp_enriched_gn <- ppau_delta_paired_median_all_cryp %>%
   filter(median_paired_delta_ppau > 0.05) %>%
   pull(gene_name)
 
-# plot gene name with ggrepel
-base_dot_plot <- liu_all_means %>%
-  filter(abundance_type == "counts_norm") %>%
-  mutate(gene_name = fct_reorder(gene_name, mean_all),
-         enr_cryp = if_else(gene_name %in% cryp_enriched_gn, T, F),
-         plot_gn = if_else(enr_cryp, gene_name, "")) %>%
-  ggplot(aes(x = log2(mean_all + 1), y = gene_name, colour = enr_cryp, label = plot_gn)) +
-  geom_point(size = rel(0.3)) +
-  geom_vline(xintercept = log2(11), linetype = "dashed") +
-  geom_vline(xintercept = log2(26), linetype = "dashed") +
-  geom_vline(xintercept = log2(51), linetype = "dashed") +
-  scale_colour_manual(values = c("#bdbdbd", "#d95f02")) +
-  theme_classic(base_size = 14) +
-  labs(title = "Cryptic-containing gene expression",
-       subtitle = "Vertical lines correpond to a mean of 10, 25 & 50 reads respectively",
-       x = "log2(mean count + 1)",
-       y = "Gene",
-       colour = "Passes enrichment criteria") +
-  theme(axis.text.y = element_blank(),
-        axis.ticks.y = element_blank(),
-        legend.position = "top")
+# label genes as passing enrichment criteria or not
+liu_all_means <- liu_all_means %>%
+  mutate(enr_cryp = if_else(gene_name %in% cryp_enriched_gn, T, F))
 
-# add gene names for enriched cryptics
-gn_dot_plot <- base_dot_plot +
-  geom_text_repel(size = rel(2.5),
-                  direction = "x",
-                  min.segment.length = 0,
-                  force = 25, max.overlaps = 100, segment.size = 0.1, segment.linetype = 2,
-                  seed = 123)
+# for each mean calculation (all samples, -ve only, +ve only)
+# make base dot plot highlighting expression of enriched cryptic-containing genes
+base_dot_plots <- colnames(liu_all_means)[str_starts(colnames(liu_all_means), "mean_")] %>%
+  set_names() %>%
+  map(~ base_dotplot(filter(liu_all_means, abundance_type == "counts_norm"), .x, glue("{.x} - Cryptic gene expression")))
 
-gn_dot_plot
-  
+# add gene names to all plots
+gn_dot_plots <- base_dot_plots %>%
+  map(~ .x +
+        geom_text_repel(size = rel(2.5),
+                        direction = "x",
+                        min.segment.length = 0,
+                        force = 25, 
+                        max.overlaps = 100, 
+                        segment.size = 0.1, 
+                        segment.linetype = 2,
+                        seed = 123)
+      )
 
-if (!dir.exists("processed")) {dir.create("processed", recursive = T)}
+# write to file
+if (!dir.exists("processed/gene_exprn")) {dir.create("processed/gene_exprn", recursive = T)}
 
-ggsave(filename = "processed/gene_exprn/2023-09-12_cryptic_gene_exprn_dot_plot_no_label.png",
-       plot = base_dot_plot,
-       height = 8,
-       width = 8,
-       units = "in",
-       dpi = "retina")
+walk2(base_dot_plots,
+      names(base_dot_plots),
+      ~ ggsave(filename = glue("processed/gene_exprn/2023-09-13_cryptic_gene_exprn_dot_plot.{.y}.no_label.png"),
+               plot = .x,
+               height = 8,
+               width = 8,
+               units = "in",
+               dpi = "retina")
+      )
 
-ggsave(filename = "processed/gene_exprn/2023-09-12_cryptic_gene_exprn_dot_plot_gn_label.png",
-       plot = gn_dot_plot,
-       height = 8,
-       width = 8,
-       units = "in",
-       dpi = "retina")
+walk2(gn_dot_plots,
+      names(gn_dot_plots),
+      ~ ggsave(filename = glue("processed/gene_exprn/2023-09-13_cryptic_gene_exprn_dot_plot.{.y}.label.png"),
+               plot = .x,
+               height = 8,
+               width = 8,
+               units = "in",
+               dpi = "retina")
+)
 
 
-### Summary plots of gene expression for enriched vs not erniched cryptics
+### Summary plots of gene expression for enriched vs not enriched cryptics
 
-plot_liu_all_means <- liu_all_means %>%
-  filter(abundance_type == "counts_norm") %>%
-  mutate(gene_name = fct_reorder(gene_name, mean_all),
-         enr_cryp = if_else(gene_name %in% cryp_enriched_gn, T, F),
-         plot_mean = log2(mean_all + 1)) 
+cryp_enr_vs_ge_densitys <- colnames(liu_all_means)[str_starts(colnames(liu_all_means), "mean_")] %>%
+  set_names() %>%
+  map(~ ge_density_plot(filter(liu_all_means, abundance_type == "counts_norm"), .x, glue("{.x} - Cryptic gene expression")))
 
-cryp_enr_vs_ge_density <- plot_liu_all_means %>%
-  ggplot(aes(x = plot_mean, fill = enr_cryp, colour = enr_cryp)) +
-  geom_density(alpha = 0.5) +
-  scale_colour_manual(values = c("#bdbdbd", "#d95f02")) +
-  scale_fill_manual(values = c("#bdbdbd", "#d95f02")) +
-  theme_classic(base_size = 14) +
-  labs(title = "Cryptic-containing gene expression",
-       x = "log2(mean count + 1)",
-       fill = "Passes enrichment criteria",
-       colour = "Passes enrichment criteria"
-       ) +
-  theme(legend.position = "top")
+cryp_enr_vs_ge_boxplots <- colnames(liu_all_means)[str_starts(colnames(liu_all_means), "mean_")] %>%
+  set_names() %>%
+  map(~ ge_boxplot(filter(liu_all_means, abundance_type == "counts_norm"), .x, glue("{.x} - Cryptic gene expression")))
 
-cryp_enr_vs_ge_box <- plot_liu_all_means %>%
-  ggplot(aes(y = plot_mean, x = enr_cryp, colour = enr_cryp)) +
-  geom_boxplot(outlier.shape = NA) + 
-  geom_jitter() +
-  ggpubr::stat_compare_means() +
-  scale_colour_manual(values = c("#bdbdbd", "#d95f02")) +
-  scale_fill_manual(values = c("#bdbdbd", "#d95f02")) +
-  theme_classic(base_size = 14) +
-  labs(title = "Cryptic-containing gene expression",
-       x = "Passes enrichment criteria",
-       y = "log2(mean count + 1)"
-  )  +
-  guides(fill = "none",
-         colour = "none")
+# write to file
+walk2(cryp_enr_vs_ge_densitys,
+      names(cryp_enr_vs_ge_densitys),
+      ~ ggsave(filename = glue("processed/gene_exprn/2023-09-13_cryptic_gene_exprn_enr_vs_not_density_plot.{.y}.png"),
+               plot = .x,
+               height = 8,
+               width = 8,
+               units = "in",
+               dpi = "retina")
+)
 
+walk2(cryp_enr_vs_ge_boxplots,
+      names(cryp_enr_vs_ge_boxplots),
+      ~ ggsave(filename = glue("processed/gene_exprn/2023-09-13_cryptic_gene_exprn_enr_vs_not_box_plot.{.y}.png"),
+               plot = .x,
+               height = 8,
+               width = 8,
+               units = "in",
+               dpi = "retina")
+)
 
-ggsave(filename = "processed/gene_exprn/2023-09-12_cryptic_gene_exprn_enr_vs_not_density_plot.png",
-       plot = cryp_enr_vs_ge_density,
-       height = 8,
-       width = 8,
-       units = "in",
-       dpi = "retina")
-
-ggsave(filename = "processed/gene_exprn/2023-09-12_cryptic_gene_exprn_enr_vs_not_boxplot.png",
-       plot = cryp_enr_vs_ge_box,
-       height = 8,
-       width = 8,
-       units = "in",
-       dpi = "retina")
-
-
-#
-
-liu_all_means %>%
-  filter(abundance_type == "counts_norm") %>%
-  mutate(gene_name = fct_reorder(gene_name, mean_all),
-         enr_cryp = if_else(gene_name %in% cryp_enriched_gn, T, F),
-         plot_gn = if_else(enr_cryp, gene_name, "")) %>%
-  ggplot(aes(x = log2(mean_all + 1), y = gene_name, colour = enr_cryp, label = plot_gn)) +
-  geom_point(size = rel(0.1)) +
-  geom_vline(xintercept = log2(11), linetype = "dashed", alpha = 0.5) +
-  geom_vline(xintercept = log2(26), linetype = "dashed", alpha = 0.5) +
-  geom_vline(xintercept = log2(51), linetype = "dashed", alpha = 0.5) +
-  geom_text_repel(size = rel(2),
-                  direction = "x",
-                  min.segment.length = 0,
-                  force = 25, max.overlaps = 100, segment.size = 0.1, segment.linetype = 2,
-                  seed = 123) +
-  scale_colour_manual(values = c("#bdbdbd", "#d95f02")) +
-  theme_classic(base_size = 14) +
-  labs(title = "Cryptic-containing gene expression",
-       subtitle = "lines left-right are mean of 10, 25 & 50 reads in gene",
-       colour = "Passes enrichment criteria") +
-  theme(axis.text.y = element_blank(),
-        axis.ticks.y = element_blank(),
-        legend.position = "top")
 
 
 
@@ -251,76 +275,102 @@ liu_all_means %>%
 # - Are 'enriched' genes over-represented in highly expressed genes? See where they pop up in curve, possibly histograms of expression
 # - could make similar cumulative distribution plot (maybe just individual dots) coloured by event type as well
 
-liu_all_means %>%
-  filter(abundance_type == "counts_norm") %>%
-  mutate(gene_name = fct_reorder(gene_name, mean_all),
-         enr_cryp = if_else(gene_name %in% cryp_enriched_gn, T, F)) %>%
-  ggplot(aes(x = log2(mean_all + 1), y = gene_name, colour = enr_cryp)) +
-  geom_point(size = rel(0.1)) +
-  geom_vline(xintercept = log2(11), linetype = "dashed") +
-  geom_vline(xintercept = log2(26), linetype = "dashed") +
-  geom_vline(xintercept = log2(51), linetype = "dashed") +
-  theme_bw(base_size = 14) +
-  labs(title = "Cryptic-containing gene expression") +
-  theme(axis.text.y = element_blank(),
-        axis.ticks.y = element_blank())
 
-
-liu_all_means %>%
-  filter(abundance_type == "counts_norm") %>%
-  mutate(gene_name = fct_reorder(gene_name, mean_all),
-         enr_cryp = if_else(gene_name %in% cryp_enriched_gn, T, F)) %>%
-  ggplot(aes(x = log2(mean_all + 1), fill = enr_cryp, colour = enr_cryp)) +
-  geom_density(alpha = 0.5)
-
-liu_all_means %>%
-  filter(abundance_type == "counts_norm") %>%
-  mutate(gene_name = fct_reorder(gene_name, mean_all),
-         enr_cryp = if_else(gene_name %in% cryp_enriched_gn, T, F)) %>%
-  ggplot(aes(y = log2(mean_all + 1), x = enr_cryp, colour = enr_cryp)) +
-  geom_boxplot(outlier.shape = NA) + 
-  geom_jitter(alpha = 0.5) +
-  ggpubr::stat_compare_means()
-
-# atte,pt to plot gene name alongside cryptics
-liu_all_means %>%
-  filter(abundance_type == "counts_norm") %>%
-  mutate(gene_name = fct_reorder(gene_name, mean_all),
-         enr_cryp = if_else(gene_name %in% cryp_enriched_gn, T, F),
-         plot_gn = if_else(enr_cryp, gene_name, "")) %>%
-  ggplot(aes(x = log2(mean_all + 1), y = gene_name, colour = enr_cryp, label = plot_gn)) +
-  geom_point(size = rel(0.1)) +
-  geom_text(size = rel(2), nudge_x = 5) +
-  geom_vline(xintercept = log2(11), linetype = "dashed") +
-  geom_vline(xintercept = log2(26), linetype = "dashed") +
-  geom_vline(xintercept = log2(51), linetype = "dashed") +
-  scale_colour_manual(values = c("#bdbdbd", "#d95f02")) +
-  theme_classic(base_size = 14) +
-  labs(title = "Cryptic-containing gene expression") +
-  theme(axis.text.y = element_blank(),
-        axis.ticks.y = element_blank())
-  
-# plot gene name with ggrepel
-  liu_all_means %>%
-    filter(abundance_type == "counts_norm") %>%
-    mutate(gene_name = fct_reorder(gene_name, mean_all),
-           enr_cryp = if_else(gene_name %in% cryp_enriched_gn, T, F),
-           plot_gn = if_else(enr_cryp, gene_name, "")) %>%
-    ggplot(aes(x = log2(mean_all + 1), y = gene_name, colour = enr_cryp, label = plot_gn)) +
-    geom_point(size = rel(0.1)) +
-    geom_vline(xintercept = log2(11), linetype = "dashed", alpha = 0.5) +
-    geom_vline(xintercept = log2(26), linetype = "dashed", alpha = 0.5) +
-    geom_vline(xintercept = log2(51), linetype = "dashed", alpha = 0.5) +
-    geom_text_repel(size = rel(2),
-                    direction = "x",
-                    min.segment.length = 0,
-                    force = 25, max.overlaps = 100, segment.size = 0.1, segment.linetype = 2,
-                    seed = 123) +
-    scale_colour_manual(values = c("#bdbdbd", "#d95f02")) +
-    theme_classic(base_size = 14) +
-    labs(title = "Cryptic-containing gene expression",
-         subtitle = "lines left-right are mean of 10, 25 & 50 reads in gene",
-         colour = "Passes enrichment criteria") +
-    theme(axis.text.y = element_blank(),
-          axis.ticks.y = element_blank(),
-          legend.position = "top")
+# liu_all_means %>%
+#   filter(abundance_type == "counts_norm") %>%
+#   mutate(gene_name = fct_reorder(gene_name, mean_all),
+#          enr_cryp = if_else(gene_name %in% cryp_enriched_gn, T, F),
+#          plot_gn = if_else(enr_cryp, gene_name, "")) %>%
+#   ggplot(aes(x = log2(mean_all + 1), y = gene_name, colour = enr_cryp, label = plot_gn)) +
+#   geom_point(size = rel(0.1)) +
+#   geom_vline(xintercept = log2(11), linetype = "dashed", alpha = 0.5) +
+#   geom_vline(xintercept = log2(26), linetype = "dashed", alpha = 0.5) +
+#   geom_vline(xintercept = log2(51), linetype = "dashed", alpha = 0.5) +
+#   geom_text_repel(size = rel(2),
+#                   direction = "x",
+#                   min.segment.length = 0,
+#                   force = 25, max.overlaps = 100, segment.size = 0.1, segment.linetype = 2,
+#                   seed = 123) +
+#   scale_colour_manual(values = c("#bdbdbd", "#d95f02")) +
+#   theme_classic(base_size = 14) +
+#   labs(title = "Cryptic-containing gene expression",
+#        subtitle = "lines left-right are mean of 10, 25 & 50 reads in gene",
+#        colour = "Passes enrichment criteria") +
+#   theme(axis.text.y = element_blank(),
+#         axis.ticks.y = element_blank(),
+#         legend.position = "top")
+# 
+# 
+# liu_all_means %>%
+#   filter(abundance_type == "counts_norm") %>%
+#   mutate(gene_name = fct_reorder(gene_name, mean_all),
+#          enr_cryp = if_else(gene_name %in% cryp_enriched_gn, T, F)) %>%
+#   ggplot(aes(x = log2(mean_all + 1), y = gene_name, colour = enr_cryp)) +
+#   geom_point(size = rel(0.1)) +
+#   geom_vline(xintercept = log2(11), linetype = "dashed") +
+#   geom_vline(xintercept = log2(26), linetype = "dashed") +
+#   geom_vline(xintercept = log2(51), linetype = "dashed") +
+#   theme_bw(base_size = 14) +
+#   labs(title = "Cryptic-containing gene expression") +
+#   theme(axis.text.y = element_blank(),
+#         axis.ticks.y = element_blank())
+# 
+# 
+# liu_all_means %>%
+#   filter(abundance_type == "counts_norm") %>%
+#   mutate(gene_name = fct_reorder(gene_name, mean_all),
+#          enr_cryp = if_else(gene_name %in% cryp_enriched_gn, T, F)) %>%
+#   ggplot(aes(x = log2(mean_all + 1), fill = enr_cryp, colour = enr_cryp)) +
+#   geom_density(alpha = 0.5)
+# 
+# liu_all_means %>%
+#   filter(abundance_type == "counts_norm") %>%
+#   mutate(gene_name = fct_reorder(gene_name, mean_all),
+#          enr_cryp = if_else(gene_name %in% cryp_enriched_gn, T, F)) %>%
+#   ggplot(aes(y = log2(mean_all + 1), x = enr_cryp, colour = enr_cryp)) +
+#   geom_boxplot(outlier.shape = NA) + 
+#   geom_jitter(alpha = 0.5) +
+#   ggpubr::stat_compare_means()
+# 
+# # atte,pt to plot gene name alongside cryptics
+# liu_all_means %>%
+#   filter(abundance_type == "counts_norm") %>%
+#   mutate(gene_name = fct_reorder(gene_name, mean_all),
+#          enr_cryp = if_else(gene_name %in% cryp_enriched_gn, T, F),
+#          plot_gn = if_else(enr_cryp, gene_name, "")) %>%
+#   ggplot(aes(x = log2(mean_all + 1), y = gene_name, colour = enr_cryp, label = plot_gn)) +
+#   geom_point(size = rel(0.1)) +
+#   geom_text(size = rel(2), nudge_x = 5) +
+#   geom_vline(xintercept = log2(11), linetype = "dashed") +
+#   geom_vline(xintercept = log2(26), linetype = "dashed") +
+#   geom_vline(xintercept = log2(51), linetype = "dashed") +
+#   scale_colour_manual(values = c("#bdbdbd", "#d95f02")) +
+#   theme_classic(base_size = 14) +
+#   labs(title = "Cryptic-containing gene expression") +
+#   theme(axis.text.y = element_blank(),
+#         axis.ticks.y = element_blank())
+#   
+# # plot gene name with ggrepel
+#   liu_all_means %>%
+#     filter(abundance_type == "counts_norm") %>%
+#     mutate(gene_name = fct_reorder(gene_name, mean_all),
+#            enr_cryp = if_else(gene_name %in% cryp_enriched_gn, T, F),
+#            plot_gn = if_else(enr_cryp, gene_name, "")) %>%
+#     ggplot(aes(x = log2(mean_all + 1), y = gene_name, colour = enr_cryp, label = plot_gn)) +
+#     geom_point(size = rel(0.1)) +
+#     geom_vline(xintercept = log2(11), linetype = "dashed", alpha = 0.5) +
+#     geom_vline(xintercept = log2(26), linetype = "dashed", alpha = 0.5) +
+#     geom_vline(xintercept = log2(51), linetype = "dashed", alpha = 0.5) +
+#     geom_text_repel(size = rel(2),
+#                     direction = "x",
+#                     min.segment.length = 0,
+#                     force = 25, max.overlaps = 100, segment.size = 0.1, segment.linetype = 2,
+#                     seed = 123) +
+#     scale_colour_manual(values = c("#bdbdbd", "#d95f02")) +
+#     theme_classic(base_size = 14) +
+#     labs(title = "Cryptic-containing gene expression",
+#          subtitle = "lines left-right are mean of 10, 25 & 50 reads in gene",
+#          colour = "Passes enrichment criteria") +
+#     theme(axis.text.y = element_blank(),
+#           axis.ticks.y = element_blank(),
+#           legend.position = "top")
