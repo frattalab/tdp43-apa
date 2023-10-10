b66d7d6f67e9aea710c19ba551dd29dac1fa338f
SamBryce-Smith <samuelbrycesmith@gmail.com>
2023-10-10 18:48:10 +0100

    output final SVGs for slam-seq half-life curves

--- slamseq/estimate_half_life.R
+++ slamseq/estimate_half_life.R
@@ -5,10 +5,45 @@ library(purrr)
 library(ggplot2)
 library(grandR)
 library(ggrastr)
-
+library(glue)
 give.n <- function(x){
     return(c(y = median(x)*1.05, label = length(x)))
 }
+
+#' function to plot fitted half life curves in two conditions for specific genes
+plot_hl <- function(go_of_interest, fitted = all_old_rna_long_fitted, real = all_old_rna_long_real, kinetic = corticali3_kinetic) {
+  
+  # extract CTL & KD half lives for gene of interest
+  go_kinetic <- filter(kinetic, Symbol == go_of_interest)
+  
+  ctl_hl <- pull(go_kinetic, `kinetics.Ctrl.Half-life`)
+  kd_hl <- pull(go_kinetic, `kinetics.TDPKD.Half-life`)
+  
+  ggplot() + 
+    geom_line(aes(x = time,y = Value,color = Condition),data = all_old_rna_long_fitted[gene == go_of_interest],linewidth = 1.5) + 
+    geom_point(aes(x = time,y = Value,color = Condition),data = all_old_rna_long_real[gene == go_of_interest], show.legend =  FALSE) + 
+    geom_pointrange(aes(x = time,color = Condition,ymin=lower, ymax=upper,y = Value),data = all_old_rna_long_real[gene == go_of_interest],
+                    show.legend = FALSE) + 
+    ylab("Unlabelled RNA") + 
+    xlab("4SU labelling time") + 
+    scale_color_manual(values=c('#999999','#E69F00')) +
+    scale_x_continuous(breaks = c(0, 1, 4, 8, 12, 24)) + 
+    scale_y_continuous(limits = c(0,1500),
+                       breaks = seq(0,1500,250)) +
+    geom_text(aes(x = 24, y = 1500, label = glue("Control: {round(ctl_hl,2)} hr")),
+              hjust = 1, #right align
+              size = 7.5, colour = "#999999") +
+    geom_text(aes(x = 24, y = 1400, label = glue("TDP-43 KD: {round(kd_hl,2)} hr")),
+              hjust = 1, #right align
+              size = 7.5, colour = "#E69F00") +
+    theme_classic(base_size = 20) +
+    # ggpubr::theme_pubr(base_size = 20) +
+    theme(legend.title = element_blank(),
+          legend.position = "top") +
+    facet_wrap(~gene)
+  
+}
+
 
 corticali3_gran = ReadGRAND('data/grandslam_exp1_grandR.tsv',
                       design=c(Design$Condition,
@@ -90,7 +125,7 @@ corticali3_norm = ComputeNtrCI(corticali3_norm)
 all_old_rna_long_real = data.table()
 all_old_rna_long_fitted = data.table()
 
-this_bois = c("ELK1","SIX3","TLX1")    
+this_bois = c("ELK1","SIX3","TLX1", "BRINP2")    
 
 for(g in this_bois){
     
@@ -109,25 +144,26 @@ for(g in this_bois){
 
 
 
-go_of_interest = 'TLX1'
+genes_of_interest <- this_bois %>% set_names()
 
-ggplot() + 
-    geom_line(aes(x = time,y = Value,color = Condition),data = all_old_rna_long_fitted[gene == go_of_interest],size = 2) + 
-    geom_point(aes(x = time,y = Value,color = Condition),data = all_old_rna_long_real[gene == go_of_interest],show_guide = FALSE) + 
-    geom_pointrange(aes(x = time,color = Condition,ymin=lower, ymax=upper,y = Value),data = all_old_rna_long_real[gene == go_of_interest],show_guide = FALSE) + 
-    ggpubr::theme_pubr() + 
-    ylab("Unlabelled RNA") + 
-    xlab("4SU labelling time") + 
-    scale_color_manual(values=c('#999999','#E69F00')) + 
-    # annotate("text",  x=10, y = 1000, label = glue::glue("Control Half-Life  {round(half_life_control_estimate,1)} h"),vjust=1, hjust=0) +
-    # annotate("text",  x=10, y = 900, label = glue::glue("TDP-43 KD Half-Life {round(half_life_tdpkd_estimate,1)} h"),vjust=1, hjust=0) +
-    ggpubr::theme_pubr() +
-    ggpubr::labs_pubr() +
-    theme(legend.title = element_blank()) +
-    scale_x_continuous(breaks = c(0, 1, 4, 8, 12, 24)) + 
-    facet_wrap(~gene)
+# plots for each 3'UTR cryptic
+goi_hl_curves <- map(genes_of_interest,
+    ~ plot_hl(.x))
 
 
 if (!dir.exists("processed")) {dir.create("processed", recursive = T)}
 
 fwrite(corticali3_kinetic, "processed/2023-08-22_i3cortical_slamseq_grandr_kinetics.tsv",sep = "\t",col.names = T)
+
+walk2(goi_hl_curves,
+      names(goi_hl_curves),
+      ~ ggsave(filename = glue("2023-10-10_i3cortical_hl_curve_{.y}.svg"),
+               plot = .x,
+               device = svg,
+               path = "processed/",
+               width = 6,
+               height = 6,
+               units = "in",
+               dpi = "retina"
+               )
+      )
