3b83ad5eb5e0ea0e97efb9921b9f54ff91e7b6ad
SamBryce-Smith <samuelbrycesmith@gmail.com>
2023-10-27 13:00:33 +0100

    intial commit code to run DEXSeq on Liu FACS quantification

---
+++ postmortem/run_dexseq.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# Need this conda env active - envs/papa_r.yaml - papa_pipeline_r
+
+sample_tbl="data/liu_facs/liu_facs_sample_sheet_dexseq_meta.csv"
+formulas="data/liu_facs/liu_facs_dexseq_formulas.txt"
+counts="processed/liu_facs/2023-10-27_summarised_pas_quantification_cryptics.counts.tsv"
+le2gene="processed/2023-06-22_cryptics_plus_decoys.decoys_full_fix_tx2le.le2gene.tsv"
+out_prefix="processed/liu_facs/2023-10-27_liu_facs_covariate_TDPneg_TDPpos"
+base_key="TDPpos"
+contrast_key="TDPneg"
+contrast_name="TDPneg_vs_TDPpos"
+
+
+Rscript scripts/run_dexseq.R \
+-i $counts \
+-g $le2gene \
+-s $sample_tbl \
+-f $formulas \
+--base-key $base_key \
+--contrast-key $contrast_key \
+--constrast-name $contrast_name \
+-o $out_prefix &> ${out_prefix}.run_dexseq.log
--- postmortem/summarise_quant.sh
+++ postmortem/summarise_quant.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-# Need this conda env active - envs/papa_r.yaml
+# Need this conda env active - envs/papa_r.yaml - papa_pipeline_r
 
 script="scripts/tx_to_polya_quant.R"
-sample_tbl="data/liu_facs_papa_sample_sheet.csv"
-salmon_dir="data/liu_facs/2023-06-21_gencode_v40_cryptics_decoys_full/salmon_quant"
+sample_tbl="data/liu_facs/liu_facs_sample_sheet_dexseq_meta.csv"
+salmon_dir="data/liu_facs/2023-08-23_salmon_reference_filtered_cryptic_le_combined/salmon_quant"
 tx2le="processed/2023-06-22_cryptics_plus_decoys.decoys_full_fix_tx2le.tx2le.tsv"
 le2gene="processed/2023-06-22_cryptics_plus_decoys.decoys_full_fix_tx2le.tx2gene.tsv"
-out_prefix="processed/2023-06-21_summarised_pas_quantification_cryptics_fix_tx2le"
+out_prefix="processed/liu_facs/2023-10-27_summarised_pas_quantification_cryptics"
 
 Rscript $script \
 -s $sample_tbl \
