0161193fdf4e43a75b31b841243d1b293af5808d
SamBryce-Smith <samuelbrycesmith@gmail.com>
2023-11-10 16:29:13 +0000

    save rank plots. Add boxplots of dbrn of kmer ranks across comparisons

--- motifs/scripts/papa_explore_peka.R
+++ motifs/scripts/papa_explore_peka.R
@@ -1,7 +1,7 @@
 library(tidyverse)
 
-dbrn_tbl <- read_tsv("processed/peka/papa/2023-11-02_papa_cryptics_kmer6_window_200_distal_window_250.cleaned_6mer_distribution_genome.tsv")
-simp_dbrn_tbl <- read_tsv("processed/peka/papa/2023-11-02_papa_cryptics_kmer6_window_200_distal_window_250.cleaned_6mer_distribution_genome_simple.tsv")
+dbrn_tbl <- read_tsv("processed/peka/papa/2023-11-03_papa_cryptics_kmer6_window_250_distal_window_500.cleaned_6mer_distribution_genome.tsv")
+simp_dbrn_tbl <- read_tsv("processed/peka/papa/2023-11-03_papa_cryptics_kmer6_window_250_distal_window_500.cleaned_6mer_distribution_genome_simple.tsv")
 
 
 # adjust p-values across all comparisons
@@ -63,9 +63,27 @@ kmer_ranks %>%
   arrange(median_rank)
 
 
+# plot PEKA score distributions across comparisons
+# Essentially a Z-score, so good to check if looks normally distributed and pvalues are valid
+simp_dbrn_tbl %>%
+  ggplot(aes(x = PEKA_score)) +
+  facet_wrap("~ comparison_name") +
+  geom_density() +
+  theme_bw()
+
+# They all have peak around 0, but distribution has a really long extreme right-tail
+
+simp_dbrn_tbl %>%
+  ggplot(aes(x = pvalue)) +
+  facet_wrap("~ comparison_name", scales = "free_y") +
+  geom_histogram(bins = 200) +
+  theme_bw()
+
+# some peak at low p-values, but generally have a weird peak a ~ 0.75
+
 # plot median ranks of top n motifs across all experiments
-ave_all_kmer_ranks %>%
-  slice_min(median_rank, n = 20) %>%
+median_all_kmer_ranks_bar <- ave_all_kmer_ranks %>%
+  slice_min(median_rank, n = 25) %>%
   ungroup() %>%
   mutate(kmer = fct_reorder(kmer, median_rank)) %>%
   ggplot(aes(x = kmer, y = median_rank)) +
@@ -73,13 +91,50 @@ ave_all_kmer_ranks %>%
   scale_y_continuous(breaks = seq(0,500,100),
                      minor_breaks = seq(0,500,50)) +
   theme_bw(base_size = 20) +
-  labs(title = "Top 20 most enriched 6mers across comparisons",
+  labs(title = "Top 25 most enriched 6mers across comparisons",
        x = "kmer",
        y = "Median rank of PEKA score") +
   theme(axis.text.x = element_text(angle = 90))
 
-# plot ranks of top 20 in each comparison
-kmer_ranks %>%
+median_all_kmer_ranks_bar
+
+ave_all_kmer_ranks %>%
+  # pick top 25 enriched by median rank across comparisons ('generally enriched motifs')
+  slice_min(median_rank, n = 25) %>%
+  left_join(kmer_ranks, by = "kmer") %>%
+  # sort axis from min ranks to max ranks
+  mutate(kmer = fct_reorder(kmer, median_rank)) %>%
+  ggplot(aes(x = kmer, y = log2(peka_rank))) +
+  geom_boxplot(outlier.shape = NA) +
+  geom_jitter(width = 0.2) +
+  theme_bw(base_size = 20) +
+  labs(title = "Top 25 most enriched 6mers across comparisons",
+       x = "kmer",
+       y = "log2 Median rank of PEKA score") +
+  theme(axis.text.x = element_text(angle = 90))
+
+median_all_kmer_ranks_box_colour <- ave_all_kmer_ranks %>%
+  # pick top 25 enriched by median rank across comparisons ('generally enriched motifs')
+  slice_min(median_rank, n = 25) %>%
+  left_join(kmer_ranks, by = "kmer") %>%
+  # sort axis from min ranks to max ranks
+  mutate(kmer = fct_reorder(kmer, median_rank)) %>%
+  ggplot(aes(x = kmer, y = log2(peka_rank))) +
+  geom_boxplot(outlier.shape = NA) +
+  geom_jitter(aes(x = kmer, y = log2(peka_rank), colour = comparison_name),
+              width = 0.2) +
+  theme_bw(base_size = 20) +
+  labs(title = "Top 25 most enriched 6mers across comparisons",
+       x = "kmer",
+       y = "log2 Median rank of PEKA score") +
+  theme(axis.text.x = element_text(angle = 90),
+        legend.position = "bottom")
+
+median_all_kmer_ranks_box_colour
+
+
+# plot ranks of top 25 in each comparison
+top_comp_kmer_ranks_bar <- kmer_ranks %>%
   group_by(comparison_name) %>%
   slice_min(peka_rank, n = 25) %>%
   ungroup() %>%
@@ -90,6 +145,67 @@ kmer_ranks %>%
   geom_col() +
   scale_x_reordered() +
   theme_bw(base_size = 20) +
-  theme(axis.text.x = element_text(angle = 90))
+  theme(axis.text.x = element_text(angle = 90)) +
+  labs(x = "",
+       y = "Rank by PEKA score",)
+
+top_comp_kmer_ranks_bar
   
 
+# save plots
+if (!dir.exists("processed/peka/papa/plots/")) {dir.create("processed/peka/papa/plots", recursive = T)}
+
+ggsave(filename = "2023-11-10_median_peka_score_rank_bar_top25.png",
+       plot = median_all_kmer_ranks_bar,
+       device = "png",
+       path = "processed/peka/papa/plots/",
+       width = 10,
+       height = 10,
+       units = "in",
+       dpi = "retina")
+
+ggsave(filename = "2023-11-10_median_peka_score_rank_bar_top25.svg",
+       plot = median_all_kmer_ranks_bar,
+       device = svg,
+       path = "processed/peka/papa/plots/",
+       width = 10,
+       height = 10,
+       units = "in",
+       dpi = "retina")
+
+ggsave(filename = "2023-11-10_median_peka_score_rank_box_colourcomp_top25.png",
+       plot = median_all_kmer_ranks_box_colour,
+       device = "png",
+       path = "processed/peka/papa/plots/",
+       width = 10,
+       height = 10,
+       units = "in",
+       dpi = "retina")
+
+ggsave(filename = "2023-11-10_median_peka_score_rank_box_colourcomp_top25.svg",
+       plot = median_all_kmer_ranks_box_colour,
+       device = svg,
+       path = "processed/peka/papa/plots/",
+       width = 10,
+       height = 10,
+       units = "in",
+       dpi = "retina")
+
+
+ggsave(filename = "2023-11-10_percomparison_peka_score_rank_bar_top25.png",
+       plot = top_comp_kmer_ranks_bar,
+       device = "png",
+       path = "processed/peka/papa/plots/",
+       width = 12,
+       height = 12,
+       units = "in",
+       dpi = "retina")
+
+ggsave(filename = "2023-11-10_percomparison_peka_score_rank_bar_top25.svg",
+       plot = top_comp_kmer_ranks_bar,
+       device = svg,
+       path = "processed/peka/papa/plots/",
+       width = 12,
+       height = 12,
+       units = "in",
+       dpi = "retina")
