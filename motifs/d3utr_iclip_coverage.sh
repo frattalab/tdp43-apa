6ffb7a3101a3cc3e22cea35f15b6cc603d984712
SamBryce-Smith <samuelbrycesmith@gmail.com>
2023-12-08 12:45:08 +0000

    add shell script to generate updated PAS coverages

---
+++ motifs/d3utr_iclip_coverage.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+echo 'iCLIP coverage for 3pUTR proximal PAS, SH-SY5Y expressed background'
+
+Rscript scripts/cl_iclip_coverage.R -r processed/iclip_regions/2023-12-03_papa_cryptic_d3utr.background_shsy5y.pas.proximal.bed -i data/iCLIP/tardbp-shsy5y.concat.sort.chr.bed -c data/GRCh38.primary_assembly.genome.chromsizes.txt -w 500 -o processed/iclip_maps/coverage/background_shsy5y/d3utr/proximal -p /home/sam/mambaforge-pypy3/envs/pybioinfo/bin/bedtools
+
+echo 'iCLIP coverage for 3pUTR distal PAS, SH-SY5Y expressed background'
+
+Rscript scripts/cl_iclip_coverage.R -r processed/iclip_regions/2023-12-03_papa_cryptic_d3utr.background_shsy5y.pas.distal.bed -i data/iCLIP/tardbp-shsy5y.concat.sort.chr.bed -c data/GRCh38.primary_assembly.genome.chromsizes.txt -w 500 -o processed/iclip_maps/coverage/background_shsy5y/d3utr/distal -p /home/sam/mambaforge-pypy3/envs/pybioinfo/bin/bedtools
+
