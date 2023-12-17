1cee4a40c22dd35e284ffb75fa6348a92e3cc147
SamBryce-Smith <samuelbrycesmith@gmail.com>
2023-12-17 16:13:37 +0000

    update iclip coverage

--- motifs/spliced_iclip_coverage.sh
+++ motifs/spliced_iclip_coverage.sh
@@ -2,16 +2,8 @@
 
 echo 'iCLIP coverage for spliced exon starts, SH-SY5Y expressed background'
 
-Rscript scripts/cl_iclip_coverage.R -r processed/iclip_regions/2023-11-24_papa_cryptic_spliced.background_shsy5y.le_start.bed -i data/iCLIP/tardbp-shsy5y.concat.sort.chr.bed -c data/GRCh38.primary_assembly.genome.chromsizes.txt -w 500 -o processed/iclip_maps/coverage/background_shsy5y/spliced/le_start -p /home/sam/mambaforge-pypy3/envs/pybioinfo/bin/bedtools
+Rscript scripts/cl_iclip_coverage.R -r processed/iclip_regions/2023-12-14_papa_cryptic_spliced.background_shsy5y.le_start.bed -i data/iCLIP/tardbp-shsy5y.concat.sort.chr.bed -c data/GRCh38.primary_assembly.genome.chromsizes.txt -w 500 -o processed/iclip_maps/coverage/background_shsy5y/spliced/le_start -p /home/sam/mambaforge-pypy3/envs/pybioinfo/bin/bedtools
 
 echo 'iCLIP coverage for spliced pas, SH-SY5Y expressed background'
 
-Rscript scripts/cl_iclip_coverage.R -r processed/iclip_regions/2023-11-24_papa_cryptic_spliced.background_shsy5y.pas.bed -i data/iCLIP/tardbp-shsy5y.concat.sort.chr.bed -c data/GRCh38.primary_assembly.genome.chromsizes.txt -w 500 -o processed/iclip_maps/coverage/background_shsy5y/spliced/pas -p /home/sam/mambaforge-pypy3/envs/pybioinfo/bin/bedtools
-
-echo 'iCLIP coverage for spliced exon starts, all background'
-
-Rscript scripts/cl_iclip_coverage.R -r processed/iclip_regions/2023-11-24_papa_cryptic_spliced.background_all.le_start.bed -i data/iCLIP/tardbp-shsy5y.concat.sort.chr.bed -c data/GRCh38.primary_assembly.genome.chromsizes.txt -w 500 -o processed/iclip_maps/coverage/background_all/spliced/le_start -p /home/sam/mambaforge-pypy3/envs/pybioinfo/bin/bedtools
-
-echo 'iCLIP coverage for spliced pas, all background'
-
-Rscript scripts/cl_iclip_coverage.R -r processed/iclip_regions/2023-11-24_papa_cryptic_spliced.background_all.pas.bed -i data/iCLIP/tardbp-shsy5y.concat.sort.chr.bed -c data/GRCh38.primary_assembly.genome.chromsizes.txt -w 500 -o processed/iclip_maps/coverage/background_all/spliced/pas -p /home/sam/mambaforge-pypy3/envs/pybioinfo/bin/bedtools
+Rscript scripts/cl_iclip_coverage.R -r processed/iclip_regions/2023-12-14_papa_cryptic_spliced.background_shsy5y.pas.bed -i data/iCLIP/tardbp-shsy5y.concat.sort.chr.bed -c data/GRCh38.primary_assembly.genome.chromsizes.txt -w 500 -o processed/iclip_maps/coverage/background_shsy5y/spliced/pas -p /home/sam/mambaforge-pypy3/envs/pybioinfo/bin/bedtools
