cc4e0218ec3cfd8e9de15022c97ce0e9b5bb6db7
SamBryce-Smith <samuelbrycesmith@gmail.com>
2023-12-18 13:15:16 +0000

    try to calculate CIs for motif coverage

--- motifs/scripts/papa_plot_cv_coverage.R
+++ motifs/scripts/papa_plot_cv_coverage.R
@@ -147,6 +147,151 @@ walk2(.x = separate_sum_maps_fixed,
 )
 
 
+####
+
+event_counts <- read_tsv("processed/iclip_regions/2023-12-14_papa_bleedthrough_spliced.event_counts.tsv")
+# subset to bleedtrhoughs
+event_counts_bld <- event_counts %>%
+  filter(event_type == "bleed_out_bed_shsy5y_all") %>%
+  mutate(comparison_name = "bleedthrough_pas") %>%
+  select(comparison_name, group = reg_status, count)
+
+
+
+# Plots with confidence intervals 
+df <- dbrn_tbls_sum$`YG-containing-motifs` %>%
+  # filter() %>% str_starts(comparison_name, "^bleedthrough")
+  filter(comparison_name == "bleedthrough_pas") %>%
+  mutate(group = if_else(group == "foreground", "cryptic", group)) %>%
+  left_join(event_counts_bld, by = c("comparison_name", "group")) %>%
+  group_by(comparison_name, group, rel_posn) %>%
+  # need to know number of events that have binding, so can generate the distribution of 1s and 0s for each group
+  # then can calculate sd and confidence intervals properly
+  summarise(occur = (rel_occur / 100) * count,
+          rel_occur_est = occur / count,
+          rel_occur_orig = unique(rel_occur),
+          count_orig = unique(count)) %>%
+  ungroup()
+
+
+
+
+# get n rows for number of events in each group
+posns <- df %>% 
+select(comparison_name, group, rel_posn, count_orig) %>%
+  uncount(count_orig) %>%
+  group_by(comparison_name, group, rel_posn) %>%
+  mutate(row_n = row_number()) %>%
+  ungroup()
+
+# make the individual counts (1 row per event)
+occur_dbrn <- df %>%
+  # group_by(comparison_name, group, rel_posn) %>%
+  select(comparison_name, group, rel_posn, occur) %>%
+  mutate(occur = round(occur)) %>%
+  filter(occur != 0) %>%
+  # duplicate the rows base 
+  uncount(occur) %>%
+  # these 
+  mutate(occur = 1) %>%
+  group_by(comparison_name, group, rel_posn) %>%
+  mutate(row_n = row_number()) %>%
+  ungroup()
+
+# combine two, so join and set occur to 1 where matches, otherwise fill with 0 (full dbrn)
+posns_occur <- left_join(posns, occur_dbrn, by = c("comparison_name", "group", "rel_posn", "row_n")) %>%
+  replace_na(list(occur = 0))
+
+# no calculate intervals
+summarised_dbrn <- posns_occur %>%
+  group_by(comparison_name, group, rel_posn) %>%
+  summarize(avg_coverage = mean(occur),
+            se = sd(occur) / sqrt(n()),
+            # 95 % confidence intervals
+            lwr = avg_coverage - 1.96*se,
+            upr = avg_coverage + 1.96*se,
+            n_overlaps = sum(occur),
+            n_events = n(),
+            frac_overlaps = n_overlaps / n_events) %>%
+  ungroup()
+  
+  
+
+plot_coverage <- function(df, ci_se_mult = 1.96, event_col = "plot_type", group_col = "plot_cryptic", facet_ncol = 2, fill_colours = c("#000000", "#d95f02"), line_colours = c("#000000", "#d95f02"), fill_lab = "", colour_lab = "", title_lab = "") {
+  
+  # generate confidence interval values
+  plot_df <- plot_coverage_df(df, ci_se_mult, event_col, group_col)
+  
+  plot_df %>%
+    ggplot(aes(x = position, y = avg_coverage, color=!!sym(group_col), fill=!!sym(group_col))) +
+    geom_smooth(method = "loess", span=0.2, se = F) +
+    geom_ribbon(aes(ymin = ymin_smooth,  ymax = ymax_smooth, fill = !!sym(group_col)),
+                alpha = 0.31) +
+    xlab("Position") +
+    ylab("Average Coverage") +
+    geom_vline(xintercept = 500, linetype = "dashed", alpha = 0.5) +
+    facet_wrap(event_col, ncol = facet_ncol, scales = "fixed") +
+    scale_x_continuous(
+      limits = c(0, 1001),
+      breaks = seq(0,1000,100),
+      labels = as.character(seq(-500,500,100))
+    ) +
+    scale_y_continuous(limits = c(NA, 0.5),
+                       breaks = seq(0, 0.5, 0.05)) +
+    scale_fill_manual(values = fill_colours) +
+    scale_color_manual(values = line_colours) +
+    theme_bw(base_size = 20) +
+    theme(legend.position = "top",
+          axis.text = element_text(size = rel(1.25)),
+          axis.title = element_text(size = rel(1.25)),
+          strip.text = element_text(size = rel(1.25))
+    ) +
+    labs(fill = fill_lab, colour = colour_lab, title = title_lab)
+  
+  
+}
+
+
+plot_coverage_df <- function(df, ci_se_mult = 1.96, event_col = "plot_type", group_col = "plot_cryptic") {
+  
+  group_cols <- c(event_col, group_col)
+  
+  # generate confidence interval values
+  df %>%
+    mutate(plot_ymin = avg_coverage - (ci_se_mult*se),
+           plot_ymax = avg_coverage + (ci_se_mult*se)) %>%
+    group_by(across(all_of(group_cols))) %>%
+    mutate(ymin_smooth = stats::predict(loess(plot_ymin~position, span=0.2)),
+           ymax_smooth = stats::predict(loess(plot_ymax~position, span=0.2))) %>%
+    ungroup()
+  
+}
+
+rename(summarised_dbrn, position = rel_posn) %>%
+  mutate(plot_cryptic = if_else(group == "cryptic", "Cryptic", "Background"),
+         plot_type = "PAS",
+         position = position + 500
+  ) %>%
+  plot_coverage(ci_se_mult = 1)
+
+
+
+rename(summarised_dbrn, position = rel_posn) %>%
+  mutate(plot_cryptic = if_else(group == "cryptic", "Cryptic", "Background"),
+         plot_type = "PAS",
+         position = position + 500
+         ) %>%
+plot_coverage(ci_se_mult = 1) +
+  geom_line(colour = "blue")
+
+rename(summarised_dbrn, position = rel_posn) %>%
+  mutate(plot_cryptic = if_else(group == "cryptic", "Cryptic", "Background"),
+         plot_type = "PAS",
+         position = position + 500
+  ) %>%
+  plot_coverage_df(ci_se_mult = 1) %>%
+  View()
+
 # # map with free scales across y
 # map_freey <- plot_kmer_dbrn(mutate(dbrn_tbl, direction = plot_group),
 #                rolling_mean = T,
