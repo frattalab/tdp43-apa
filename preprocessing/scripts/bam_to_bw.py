e7b6992071d37beef5fc41b3662bbd7c50032bbf
SamBryce-Smith <samuelbrycesmith@gmail.com>
2024-06-11 15:51:28 +0100

    simplify and optimise script (only process matches to region strand, stay as pyranges for as little as required, remove unnecessary computation and metadata). Roughly 25 % reduction in peak memory on test data

--- preprocessing/scripts/bam_to_bw.py
+++ preprocessing/scripts/bam_to_bw.py
@@ -5,13 +5,15 @@ import pysam
 import pyrle
 from collections import namedtuple
 from typing import Tuple, Dict, List, Literal
+from functools import reduce
+from operator import add
 import argparse
 import sys
 
 # object to store different outputs of bam_to_strand_alignments
 Alignments = namedtuple('Alignments', 
-                        field_names=["gr", "fragments_plus", "fragments_minus", "refskip_idxs", "orphans_plus", "orphans_minus"],
-                        defaults=[None]*6, 
+                        field_names=["fragments", "orphans", "num_reads"],
+                        defaults=[None]*3, 
                         )
 
 # Object to store region coordinates to provide to pysam.AlignmentFile.fetch()
@@ -39,31 +41,6 @@ def get_chromsizes_dict(path: str) -> dict:
         chromsizes = {line.split('\t')[0]: line.split('\t')[1].strip() for line in infile}
 
     return chromsizes
-
-
-def cigar_to_gap_idxs(lst: List[int]) -> List[int]:
-    '''Get indexes of reference skips with respect to all gaps in an alignment
-
-    Parameters
-    ----------
-    lst : List[int]
-        operations returned by pysam.AlignedSegment.cigartuples e.g. [op for op,_ in pysam.AlignedSegment.cigartuples]
-
-    Returns
-    -------
-    List[int]
-        indexes of ref-skips in alignment with respect to all ref-skips/deletions (= gap in alignment with respect to reference)
-    '''
-    
-    # pysam docs - N (ref-skip) = 3, D (del with respect to reference) = 2
-
-    # Use a list comprehension to find the indexes where the value is 3 or 2
-    indexes = [i for i, value in enumerate(lst) if value == 3 or value == 2]
-
-    # grab the indexes of 3/ref-skip with respect to all gaps in the read alignment (3/2)
-    filtered_indexes = [i for i, lst_idx in enumerate(indexes) if lst[lst_idx] == 3]
-
-    return filtered_indexes
 
 
 def check_aln_strand(read: pysam.AlignedSegment,
@@ -156,8 +133,7 @@ def update_aligned_region(fragments: Dict[str, list], alignment: pysam.AlignedSe
 def bam_to_strand_alignments(bam_path: str,
                              region: Region,
                              strandedness: str,
-                             as_pyranges: bool = True,
-                             return_refskips: bool = True,
+                             as_pyranges: bool = False,
                              return_orphans: bool = True) -> Alignments:
     '''_summary_
 
@@ -170,9 +146,7 @@ def bam_to_strand_alignments(bam_path: str,
     strandedness : str
         _description_
     as_pyranges : bool, optional
-        _description_, by default True
-    return_refskips : bool, optional
-        _description_, by default True
+        Whether to return paired alignments + orphans as pr.PyRanges (True) or pyrle.Rle object (False), by default True
     return_orphans : bool, optional
         _description_, by default True
 
@@ -185,30 +159,18 @@ def bam_to_strand_alignments(bam_path: str,
 
     # Create a dictionary to cache read objects (per reference name/chrom)
     # {<read_name>: <pysam.AlignedSegment>}
-    read_cache_plus = {}
-    read_cache_minus = {}
+    read_cache = {}
 
-    # Store aligned regions of read pairs
-    fragments_plus = {"Chromosome": [],
+    # Store aligned regions of read pairs (both mates overlapping with interval)
+    fragments = {"Chromosome": [],
                       "Start": [],
                       "End": [],
                       "Strand": [],
-                      "read_name": [],
-                      "read_name_num": []}
-
-    fragments_minus = {"Chromosome": [],
-                       "Start": [],
-                       "End": [],
-                       "Strand": [],
-                       "read_name": [],
-                       "read_name_num": []}
-
-    # initialise a dict to store indexes of refskips for reads that have gapped alignments {read_name_num: [<refskip_idxs>]}
-    refskips = {}
+                      "read_name": []}
 
     # track the number of successfully written read pairs (per reference name/chrom)
-    read_count_plus = 0
-    read_count_minus = 0
+    # read_count_plus = 0
+
 
     for alignment in bamfile.fetch(contig=region.contig, start=region.start, stop=region.stop):
 
@@ -218,181 +180,89 @@ def bam_to_strand_alignments(bam_path: str,
             # determine putative strand of origin
             putative_strand = check_aln_strand(alignment, strandedness)
 
-            if putative_strand == 1:
-                # plus strand - check if mate is already in cache
-                if alignment.query_name in read_cache_plus:
-
+            if (putative_strand == 1 and region.strand == "+") or (putative_strand == -1 and region.strand == "-"):
+                # check if mate is already in cache
+                if alignment.query_name in read_cache:
                     # Pop the mate from the cache
-                    mate = read_cache_plus.pop(alignment.query_name)
-                    
-                    # define read_name_num - additional id of <read_name>_<r1/r2> (to allow read-specific processing)
-                    aln_name_suff = "_r1" if alignment.is_read1 else "_r2"
-                    mate_name_suff = "_r1" if mate.is_read1 else "_r2"
-
-                    aln_rname_num = alignment.query_name + aln_name_suff
-                    mate_rname_num = mate.query_name + mate_name_suff
-
-                    # get indexes of refskips with respect to all gaps in alignment
-                    if return_refskips:
-                        aln_refskips = cigar_to_gap_idxs([op for op,_ in alignment.cigartuples])
-                        mate_refskips = cigar_to_gap_idxs([op for op,_ in mate.cigartuples])
-
-                        # if alignment doesn't have any refskips then don't populate dict
-                        if len(aln_refskips) > 0:
-                            refskips[aln_rname_num] = aln_refskips
-                        
-                        if len(mate_refskips) > 0:
-                            refskips[mate_rname_num] = mate_refskips
+                    mate = read_cache.pop(alignment.query_name)
                     
                     # extract aligned coords of read and its mate to a dictionary
                     read_posns = alignment.get_blocks()
                     mate_posns = mate.get_blocks()
-
                     num_blocks = len(read_posns) + len(mate_posns)
 
+                    # Store start and end coords of current read + mate
+                    fragments["Start"].extend(tup[0] for tup in read_posns)
+                    fragments["End"].extend(tup[1] for tup in read_posns)
 
-                    # Store start and end coords of aligned segments
-                    fragments_plus["Start"].extend(tup[0] for tup in read_posns)
-                    fragments_plus["End"].extend(tup[1] for tup in read_posns)
-
-                    # add rname_num for alignment positions
-                    fragments_plus["read_name_num"].extend([aln_rname_num] * len(read_posns))
-
-                    # now add mate coords to dict
-                    fragments_plus["Start"].extend(tup[0] for tup in mate_posns)
-                    fragments_plus["End"].extend(tup[1] for tup in mate_posns)
-
-                    # add rname_num for mate positions
-                    fragments_plus["read_name_num"].extend([mate_rname_num] * len(mate_posns))
+                    fragments["Start"].extend(tup[0] for tup in mate_posns)
+                    fragments["End"].extend(tup[1] for tup in mate_posns)
 
                     # extend Chromosome, Strand & query name lists
-                    fragments_plus["Chromosome"].extend([region.contig] * num_blocks)
-                    fragments_plus["Strand"].extend(["+"] * num_blocks)
-                    fragments_plus["read_name"].extend([alignment.query_name] * num_blocks)
-
-                    read_count_plus += 1
+                    fragments["Chromosome"].extend([region.contig] * num_blocks)
+                    fragments["Strand"].extend([region.strand] * num_blocks)
+                    fragments["read_name"].extend([alignment.query_name] * num_blocks)
 
                 else:
                     # Add the alignment to the cache
-                    read_cache_plus[alignment.query_name] = alignment
+                    read_cache[alignment.query_name] = alignment
 
-            else:
-                # minus strand - check if mate already in cache
-                if alignment.query_name in read_cache_minus:
-
-                    # Pop the mate from the cache
-                    mate = read_cache_minus.pop(alignment.query_name)
-
-                    # define read_name_num - additional id of <read_name>_<r1/r2> (to allow read-specific processing)
-                    aln_name_suff = "_r1" if alignment.is_read1 else "_r2"
-                    mate_name_suff = "_r1" if mate.is_read1 else "_r2"
-
-                    aln_rname_num = alignment.query_name + aln_name_suff
-                    mate_rname_num = mate.query_name + mate_name_suff
-
-                    # get indexes of refskips with respect to all gaps in alignment
-                    if return_refskips:
-                        aln_refskips = cigar_to_gap_idxs([op for op,_ in alignment.cigartuples])
-                        mate_refskips = cigar_to_gap_idxs([op for op,_ in mate.cigartuples])
-
-                        # if alignment doesn't have any refskips then don't populate dict
-                        if len(aln_refskips) > 0:
-                            refskips[aln_rname_num] = aln_refskips
-                        
-                        if len(mate_refskips) > 0:
-                            refskips[mate_rname_num] = mate_refskips
-
-                    # extract aligned coords of read and its mate to a dictionary
-                    read_posns = alignment.get_blocks()
-                    mate_posns = mate.get_blocks()
-
-                    num_blocks = len(read_posns) + len(mate_posns)
-
-                    # Store start and end coords of aligned segments
-                    fragments_minus["Start"].extend(tup[0] for tup in read_posns)
-                    fragments_minus["End"].extend(tup[1] for tup in read_posns)
-
-                    # add rname_num for alignment positions
-                    fragments_minus["read_name_num"].extend([aln_rname_num] * len(read_posns))
-
-                    # now add mate coords to dict
-                    fragments_minus["Start"].extend(tup[0] for tup in mate_posns)
-                    fragments_minus["End"].extend(tup[1] for tup in mate_posns)
-                    
-                    # add rname_num for mate positions
-                    fragments_minus["read_name_num"].extend([mate_rname_num] * len(mate_posns))
-
-                    # extend Chromosome and Strand lists
-                    fragments_minus["Chromosome"].extend([region.contig] * num_blocks)
-                    fragments_minus["Strand"].extend(["-"] * num_blocks)
-                    fragments_minus["read_name"].extend([alignment.query_name] * num_blocks)
-
-                    read_count_minus += 1
-
-                else:
-                    # Add the alignment to the cache
-                    read_cache_minus[alignment.query_name] = alignment
-
-
+    # finished iterating
     bamfile.close()
 
     # construct output dict to be unpacked into Alignments namedtuple for output
     out_dict = {}
-    
-    # convert to pyranges or return dicts
-    if as_pyranges:
-        out_dict["gr"] = pr.concat([pr.from_dict(fragments_plus, int64=True), pr.from_dict(fragments_minus, int64=True)])
-    else:
-        out_dict["fragments_plus"] = fragments_plus
-        out_dict["fragments_minus"] = fragments_minus
+
+    # convert to pyranges object
+    fragments = pr.from_dict(fragments, int64=True)
+    num_frags = fragments.read_name.nunique()
+
+    if not as_pyranges:
+        # convert to PyRle object
+        fragments = gr_to_rle(fragments, merge=True, merge_by="read_name", by_strand=True)
+
+    out_dict["fragments"] = fragments
 
     if return_orphans:
-        if as_pyranges:
-            # plus strand first
-            if len(read_cache_plus) == 0:
-                out_dict["orphans_plus"] = pr.PyRanges()
-
-            else:
-                orphans_plus = {"Chromosome": [],
+        # extract info from orphans
+        orphans = {"Chromosome": [],
                       "Start": [],
                       "End": [],
-                      "Strand": [],
-                      "read_name": [],
-                      "read_name_num": []} 
-                
-                # parse out aligned regions for each orphan
-                for alignment in read_cache_plus.values():
-                    update_aligned_region(orphans_plus, alignment, "+")
+                      "Strand": []
+                      }
+        
+        # to add to final total of reads
+        num_orphans = len(read_cache)
+        
+        for alignment in read_cache.values():
+            # extract aligned coords of read and its mate to a dictionary
+            read_posns = alignment.get_blocks()
+            num_blocks = len(read_posns)
 
-                out_dict["orphans_plus"] = pr.from_dict(orphans_plus, int64=True)
+            # Store start and end coords of current read + mate
+            orphans["Start"].extend(tup[0] for tup in read_posns)
+            orphans["End"].extend(tup[1] for tup in read_posns)
 
-            # repeat for minus strand
-            if len(read_cache_minus) == 0:
-                out_dict["orphans_minus"] = pr.PyRanges()
-            
-            else:
-                orphans_minus = {"Chromosome": [],
-                      "Start": [],
-                      "End": [],
-                      "Strand": [],
-                      "read_name": [],
-                      "read_name_num": []} 
-                
-                # parse out aligned regions for each orphan
-                for alignment in read_cache_minus.values():
-                    update_aligned_region(orphans_minus, alignment, "-")
-                
-                out_dict["orphans_minus"] = pr.from_dict(orphans_minus, int64=True)
+            # extend Chromosome, Strand lists (read name irrelevant here as no overlap)
+            orphans["Chromosome"].extend([region.contig] * num_blocks)
+            orphans["Strand"].extend([region.strand] * num_blocks)
 
-        else:
-            out_dict["orphans_plus"] = read_cache_plus
-            out_dict["orphans_minus"] = read_cache_minus
+        # convert to pyranges object
+        orphans = pr.from_dict(orphans, int64=True)
 
-    if return_refskips:
-        out_dict["refskip_idxs"] = refskips
+        if not as_pyranges:
+            # convert to PyRle object
+            orphans = gr_to_rle(orphans, merge=False, by_strand=True)
+        
+        out_dict["orphans"] = orphans
+        out_dict["num_reads"] = num_frags + num_orphans
+    
+    else:
+        out_dict["num_reads"] = num_frags
+
 
     return Alignments(**out_dict)
-
+    
 
 def gr_to_rle(gr: pr.PyRanges, merge: bool = True, merge_by: str = "read_name", by_strand: bool = True, by_rpm: bool = False) -> pyrle.RleDict:
     '''_summary_
@@ -474,13 +344,12 @@ def main(bam_path: str,
     bed = pr.read_bed(bed_path)
 
     # merge regions by strand to prevent same reads being read in twice
-    # merge unstranded, because bam_to_strand_alignments does not consider the strand of the input region when finding overlapping reads (thought it probably should...)
-    bed = bed.merge(strand=False)
+    bed = bed.merge(strand=True)
 
     # Extract aligned segments for properly mapped read pairs
     # First generate 'Region' objects for each interval in bed file
     regions_dict = bed.apply(lambda df: df.apply(lambda row: Region(contig=row["Chromosome"],
-                                                     start=row["Start"], stop=row["End"]),
+                                                     start=row["Start"], stop=row["End"], strand=row["Strand"]),
                                                      axis=1
                                                      ).tolist(),
                                                     as_pyranges=False)
@@ -488,9 +357,7 @@ def main(bam_path: str,
     # print(regions_dict)
 
     # combine the values (regions for each chrom-strand pair) into a single iterable of Region objects
-    regions = []
-    for region_list in regions_dict.values():
-        regions.extend(region_list)
+    regions = [region_list for chrom_strand in regions_dict.values() for region_list in chrom_strand]
 
     num_regions = len(regions)
     print(f"Total number of regions - {num_regions}")
@@ -504,38 +371,33 @@ def main(bam_path: str,
         if (i + 1) % 100 == 0:
             print(f"Region number - {i + 1} / {num_regions}")
 
-        alns_list.append(bam_to_strand_alignments(bam_path, region, "--" + strandedness, return_refskips=False, return_orphans=keep_orphans))
+        alns_list.append(bam_to_strand_alignments(bam_path, region, "--" + strandedness, as_pyranges=False, return_orphans=keep_orphans))
 
-    # for idx, aln in enumerate(alns_list):
-    #     print(f"Number of aligned segments for {regions[idx]}: {len(aln.gr)}")
-    
-    # print(alns_list)
 
     # Now generate a combined PyRanges object of aligned blocks for all regions
     # TODO: option to rescue coverage from orphan reads if necessary
-    print("Calculating region-wide per base coverage...")
-    alns = pr.concat([aln.gr for aln in alns_list])
-
-    if keep_orphans:
-        orphans = pr.concat([aln.orphans_plus for aln in alns_list] + [aln.orphans_minus for aln in alns_list])
-        alns = pr.concat([alns, orphans])
-
-    print(f"Number of aligned segments for all reads - {len(alns)}")
-
-    # convert aligned blocks to genome-wide coverage, merging covereage by read pairs
-    cov = gr_to_rle(alns, merge=True)
-
-    # print(cov)
+    print("Done!")
+    print(f"Number of extracted reads across all regions - {sum(aln.num_reads for aln in alns_list)}")
     
-    # output to bigwig
-    print("Outputting coverage per strand to bigwig...")
-    rle_to_bigwig(cov, chromsizes, output_prefix)
+    print("Obtaining per-base coverage across all provided regions from overlapping pairs...")
+    # combine individual pyrle objects for each region into a single PyRle object
+    fragments_all = reduce(add, (aln.fragments for aln in alns_list))
+    
+    if keep_orphans:
+        # repeat for orphans
+        print("Adding per-base coverage of regions from orphan paired-end reads...")
+        orphans_all = reduce(add, (aln.orphans for aln in alns_list))
+        
+        # Combine coverage
+        fragments_all = fragments_all + orphans_all
 
+    print("Outputting coverage per strand to bigwig...")
+    rle_to_bigwig(fragments_all, chromsizes, output_prefix)
 
 
 if __name__ == "__main__":
 
-    parser = argparse.ArgumentParser(description="Extract BigWig files containing stranded RNA-seq coverage for specified intervals from a BAM file")
+    parser = argparse.ArgumentParser(description="Extract BigWig files containing stranded RNA-seq coverage of paired-end reads for specified intervals from a BAM file")
 
     parser.add_argument('-b', '--bam', dest='bam_path', required=True, help="Path to BAM file")
     parser.add_argument('-r', '--regions-bed', dest='regions_bed', required=True, help="Regions for coverage extraction in BED format")
@@ -566,4 +428,4 @@ if __name__ == "__main__":
     print("Keep orphans (i.e. properly paired, but only 1 aligning to region):", args.keep_orphans)
     print("Running analysis...")
 
-    main(args.bam_path,  args.regions_bed,  args.chromsizes_path, args.strandedness, args.output_prefix, args.keep_orphans)
+    main(args.bam_path, args.regions_bed, args.chromsizes_path, args.strandedness, args.output_prefix, args.keep_orphans)
