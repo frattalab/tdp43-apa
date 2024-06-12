47f6633ab7d011047761dd73526eef646489f862
SamBryce-Smith <samuelbrycesmith@gmail.com>
2024-06-12 10:18:40 +0100

    infer strand from file name and use in merge

--- preprocessing/scripts/add_coverage_to_bed.py
+++ preprocessing/scripts/add_coverage_to_bed.py
@@ -4,7 +4,7 @@ import pyranges as pr
 import pandas as pd
 import sys
 import argparse
-
+import os
 
 def main(coverage_tsvs: list,
          region_bed: str,
@@ -21,8 +21,20 @@ def main(coverage_tsvs: list,
         Name of output BED file containing region_bed with coverage value added to 'score' field of BED file (5th field)
     '''
 
+    strand2strand = {"plus": "+", "minus": "-"}
+
     # read in coverage TSVs output by megadepth (1 per strand)
     cov_tsvs = [pd.read_csv(cov_path, sep="\t",names=["Chromosome", "Start", "End", "Score"]) for cov_path in coverage_tsvs]
+    
+    # infer strand from the filename ('plus'/'minus', output by snakemake rule as <sample_name>.<megadepth>.<strand>.annotation.tsv)
+    strands_tsvs = [os.path.basename(cov_path).split(".")[-3] for cov_path in coverage_tsvs]
+    assert all(strand in strand2strand.keys() for strand in strands_tsvs), print(f"Invalid inferred strand from coverage TSV file names, should follow <prefix>.<plus/minus>.annotation.tsv")
+    
+    # convert plus/minus to +/-
+    strands_tsvs = [strand2strand[strand] for strand in strands_tsvs]
+
+    # Add strand as column
+    cov_tsvs = [df.assign(Strand=pd.Series([strand]*len(df))) for df, strand in zip(cov_tsvs, strands_tsvs)]
 
     # combine to single df of all regions
     cov = pd.concat(cov_tsvs, ignore_index=True)
@@ -32,7 +44,7 @@ def main(coverage_tsvs: list,
     regions.drop(columns="Score", inplace=True)
 
     # Add coverage column as score for each interval
-    regions = regions.merge(cov, on=["Chromosome", "Start", "End"], how="left", validate="one_to_one")
+    regions = regions.merge(cov, on=["Chromosome", "Start", "End", "Strand"], how="left", validate="one_to_one")
     
     # convert to BED format and output to file
     regions = pr.PyRanges(regions, int64=True).sort()
