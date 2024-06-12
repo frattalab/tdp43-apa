eab495eff9db4fb479512101cb1e42f0b719f3ef
SamBryce-Smith <samuelbrycesmith@gmail.com>
2024-06-12 13:21:32 +0100

    bar plot visualisations. Try pooling across fractions

--- riboseq/plot_fracseq.R
+++ riboseq/plot_fracseq.R
@@ -180,8 +180,196 @@ ppau_3exts_npc_cyto_normed %>%
   theme_bw(base_size = 14) +
   theme(legend.position = "bottom")
 
+# bar plot of all fractions
+ppau_3exts_npc_cyto_normed %>%
+  mutate(cytosol_normed_ppau = cytosol_normed_ppau * 100) %>%
+  mutate(group = case_when(str_detect(sample_name_clean, "cytosol") ~ "cytosol",
+                           str_detect(sample_name_clean, "monosome") ~ "monosome",
+                           str_detect(sample_name_clean, "2-4_ribosomes") ~ "2-4_ribosomes",
+                           str_detect(sample_name_clean, "5_ribosomes") ~ "5_ribosomes"),
+         group = factor(group, levels = c("cytosol",
+                                          "monosome",
+                                          "2-4_ribosomes",
+                                          "5_ribosomes"),
+         ),
+         replicate = str_remove_all(replicate, "^rep")
+         ) %>%
+  ggplot(aes(x = replicate,
+             y = cytosol_normed_ppau,
+             fill = group,
+             )) +
+  geom_col(position = "dodge") +
+  labs(title = "ELK1 cryptic PAS",
+       subtitle = "Individual fractions",
+       x = "Fraction",
+       y = "Cytosol normalised PAS usage (%)",
+       fill = "Replicate") +
+  scale_y_continuous(limits = c(0,200),
+                     breaks = seq(0,200,25)) +
+  scale_fill_brewer(type = "qual",palette = "Paired") +
+  theme_bw(base_size = 14) +
+  theme(legend.position = "bottom")
 
-  # filter(gene_name == "ELK1",
+#
+# group all 'some' fractions together vs cytosol
+#
+
+tpm <- read_tsv("processed/fracseq/2024-04-30_summarised_pas.tpm.tsv")
+# add gene_name
+tpm <- left_join(tpm, le2name, by = "le_id") %>%
+  relocate(gene_name, .after = gene_id)
+
+tpm_long <- pivot_longer(tpm,
+                          cols = -all_of(c("le_id", "gene_id", "gene_name")),
+                          names_to = "sample_name",
+                          values_to = "tpm")
+
+tpm_long_3exts <- filter(tpm_long, gene_name %in% c("ELK1", "SIX3", "TLX1")) %>%
+  mutate(sample_name_clean = str_remove_all(sample_name, "^SRR[0-9]*_"),
+         replicate = str_extract_all(sample_name_clean, "rep[0-9]$", simplify = T),
+         cell_type = str_split_i(sample_name_clean, "_", 1),
+         fraction = case_when(str_detect(sample_name_clean, "cytosol") ~ "cytosol",
+                              str_detect(sample_name_clean, "monosome") ~ "monosome",
+                              str_detect(sample_name_clean, "2-4_ribosomes") ~ "2-4_ribosomes",
+                              str_detect(sample_name_clean, "5_ribosomes") ~ "5_ribosomes"),
+         fraction = factor(fraction, levels = c("cytosol",
+                                          "monosome",
+                                          "2-4_ribosomes",
+                                          "5_ribosomes")
+                           )
+  )
+
+# pool the expression of isoforms across -some fractions and cytosol (per replicate)
+sum_tpm_some_cyto_3exts <- tpm_long_3exts %>%
+  mutate(group = if_else(fraction == "cytosol", "cytosol", "ribosome")) %>%
+  group_by(cell_type, replicate, gene_name, le_id, group) %>%
+  summarise(sum_tpm = sum(tpm)) %>%
+  ungroup()
+
+# calculate PPAu for each fraction (per replicate)
+ppau_some_cyto_3exts <- sum_tpm_some_cyto_3exts %>%
+  group_by(cell_type, replicate, group, gene_name) %>%
+  mutate(ppau = (sum_tpm / sum(sum_tpm)) * 100) %>%
+  ungroup()
+  
+
+# Normalise to cytosol usage for ELK1
+ppau_npc_elk1_some_cyto_cyto_normed <- ppau_some_cyto_3exts %>%
+  filter(cell_type == "NPC") %>%
+  select(-sum_tpm) %>%
+  pivot_wider(values_from = ppau, names_from = group, names_prefix = "ppau_") %>%
+  filter(gene_name == "ELK1",
+         str_ends(le_id, "_2")) %>%
+  # each ppau column, divide against cytosol usage
+  mutate(across(starts_with("ppau_"), ~ (.x / ppau_cytosol) * 100)) %>%
+  arrange(replicate) %>% 
+  pivot_longer(cols = starts_with("ppau_"), names_prefix = "ppau_", names_to = "group", values_to = "cytosol_normed_ppau")
+  
+
+# bar plot of cytosol normalised usage
+ppau_npc_elk1_some_cyto_cyto_normed %>%
+  ggplot(aes(x = replicate,
+             y = cytosol_normed_ppau,
+             fill = group,
+             replicate)) +
+  geom_col(position = "dodge") +
+  labs(title = "ELK1 cryptic PAS",
+       subtitle = "ribosome = monosome + polysome",
+       x = "Replicate",
+       y = "Cytosol normalised PAS usage (%)",
+       fill = "Fraction") +
+  scale_y_continuous(limits = c(0,180),
+                     breaks = seq(0,200,20)) +
+  scale_fill_brewer(type = "qual", palette = "Paired") +
+  theme_bw(base_size = 14) +
+  theme(legend.position = "bottom")
+
+# same, but plotting fractioons on x
+ppau_npc_elk1_some_cyto_cyto_normed %>%
+  ggplot(aes(fill = replicate,
+             y = cytosol_normed_ppau,
+             x = group,
+             replicate)) +
+  geom_col(position = "dodge") +
+  labs(title = "ELK1 cryptic PAS",
+       subtitle = "ribosome = monosome + polysome",
+       x = "Replicate",
+       y = "Cytosol normalised PAS usage (%)",
+       fill = "Fraction") +
+  scale_y_continuous(limits = c(0,160),
+                     breaks = seq(0,200,20)) +
+  scale_fill_brewer(type = "qual", palette = "Paired") +
+  theme_bw(base_size = 14) +
+  theme(legend.position = "bottom")
+
+
+# Repeat but for cytosol, monosome + 2-4/5+ polysomes
+sum_tpm_polysome_cyto_3exts <- tpm_long_3exts %>%
+  mutate(group = if_else(fraction %in% c("2-4_ribosomes", "5_ribosomes"),
+                         "polysome",
+                         fraction)) %>%
+  group_by(cell_type, replicate, gene_name, le_id, group) %>%
+  summarise(sum_tpm = sum(tpm)) %>%
+  ungroup()
+
+# calculate PPAu for each fraction (per replicate)
+ppau_polysome_cyto_3exts <- sum_tpm_polysome_cyto_3exts %>%
+  group_by(cell_type, replicate, group, gene_name) %>%
+  mutate(ppau = (sum_tpm / sum(sum_tpm)) * 100) %>%
+  ungroup()
+
+# Normalise to cytosol usage for ELK1
+ppau_npc_elk1_polysome_cyto_cyto_normed <- ppau_polysome_cyto_3exts %>%
+  filter(cell_type == "NPC") %>%
+  select(-sum_tpm) %>%
+  pivot_wider(values_from = ppau, names_from = group, names_prefix = "ppau_") %>%
+  filter(gene_name == "ELK1",
+         str_ends(le_id, "_2")) %>%
+  # each ppau column, divide against cytosol usage
+  mutate(across(starts_with("ppau_"), ~ (.x / ppau_cytosol) * 100)) %>%
+  arrange(replicate) %>% 
+  pivot_longer(cols = starts_with("ppau_"), names_prefix = "ppau_", names_to = "group", values_to = "cytosol_normed_ppau")
+
+ppau_npc_elk1_polysome_cyto_cyto_normed
+
+# bar plot of polysome pooled, 
+ppau_npc_elk1_polysome_cyto_cyto_normed %>%
+  ggplot(aes(x = replicate,
+             y = cytosol_normed_ppau,
+             fill = group,
+             replicate)) +
+  geom_col(position = "dodge") +
+  labs(title = "ELK1 cryptic PAS",
+       subtitle = "polysome = 2-4 & 5+ ribsomes pooled",
+       x = "Replicate",
+       y = "Cytosol normalised PAS usage (%)",
+       fill = "Fraction") +
+  scale_y_continuous(limits = c(0,200),
+                     breaks = seq(0,200,25)) +
+  scale_fill_brewer(type = "qual", palette = "Paired") +
+  theme_bw(base_size = 14) +
+  theme(legend.position = "bottom")
+
+
+# same plot, but plotting fractions on x axis
+ppau_npc_elk1_polysome_cyto_cyto_normed %>%
+  ggplot(aes(x = group,
+             y = cytosol_normed_ppau,
+             fill = replicate,
+             )) +
+  geom_col(position = "dodge") +
+  labs(title = "ELK1 cryptic PAS",
+       subtitle = "polysome = 2-4 & 5+ ribsomes pooled",
+       x = "Replicate",
+       y = "Cytosol normalised PAS usage (%)",
+       fill = "Fraction") +
+  scale_y_continuous(limits = c(0,200),
+                     breaks = seq(0,200,25)) +
+  scale_fill_brewer(type = "qual", palette = "Paired") +
+  theme_bw(base_size = 14) +
+  theme(legend.position = "bottom")
+
+# filter(gene_name == "ELK1",
   #        str_ends(le_id, "_2")) %>%
   # mutate(ppau = ppau * 100) %>%
   # ggplot(aes(x = group, y = ppau, group = replicate, shape = replicate)) +
